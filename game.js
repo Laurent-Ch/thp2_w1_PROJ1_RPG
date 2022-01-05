484b092fea52da78ef825042f11cb26ef356e2b1
Laurent-Ch <chamouleau.laurent@gmail.com>
2022-01-05 09:33:33 +0100

    first commit

---
+++ game.js
@@ -0,0 +1,13 @@
+class Game {
+  constructor(turnLeft = 10) {
+    this.turnLeft = turnLeft;
+  }
+
+  newTurn() {
+    // launch an instance of the turn Class
+    // turnleft -= 1;
+  }
+
+  // turnleft => end game
+  }
+}
