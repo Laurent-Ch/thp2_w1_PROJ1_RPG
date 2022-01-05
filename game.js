5ab0e18d1881122dce180aeb3f20a52362e36a52
Laurent-Ch <chamouleau.laurent@gmail.com>
2022-01-05 11:56:49 +0100

    end game status improved

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+const INCREASING_DISPLAY = 11;
+
 class Game {
   constructor(players) {
     this.players = players;
@@ -7,15 +9,20 @@ class Game {
   }
 
   init() {
-    while (this.turnLeft > 0) {
+    while (this.turnLeft > 0 && this.players.filter(p => p.state == "loser").length < this.players.length - 1) {
       this.newTurn();
     }
     console.log('Game over.')
-
+    let winners = this.players.filter(p => p.state != "loser");
+    winners.map(w => w.state = "winner");
+    console.log(`Winner(s): ${winners.map(w => w.name)}.`);
   }
 
   newTurn() {
+    console.log(`Turn ${INCREASING_DISPLAY - this.turnLeft} starts.`);
     new Turn(players);
     this.turnLeft -= 1;
   }
 }
+
+
--- turn.js
+++ turn.js
@@ -7,7 +7,10 @@ class Turn {
   init(players) {
     players[0].dealDamage(players[1]);
     console.log(`${players[1].name} has ${players[1].hp} left.`)
-    players[1].dealDamage(players[0]);
-    console.log(`${players[0].name} has ${players[0].hp} left.`)
+    if (players[1].hp > 0) {
+      players[1].dealDamage(players[0]);
+      console.log(`${players[0].name} has ${players[0].hp} left.`)
+    }
+    console.log("\n");
   }
 }
